ba26a7a18e04d0a3ebe1c37b8077127b658ab8bc
gundam1993 <gundam1993@gmail.com>
2017-11-26 14:07:09 +0800

    finish exercise 1-5

--- temperatureConversion.c
+++ temperatureConversion.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 /*打印华氏与摄氏温度对照表*/
-int main() {
-  int fahr, celsius;
-  float fahrf, celsiusf; // 浮点数
-  int lower, upper, step;
+// int main() {
+//   int fahr, celsius;
+//   float fahrf, celsiusf; // 浮点数
+//   int lower, upper, step;
 
-  lower = 0; // 温度的下限
-  upper = 300; // 温度上限
-  step = 20; // 步长
+//   lower = 0; // 温度的下限
+//   upper = 300; // 温度上限
+//   step = 20; // 步长
 
-  fahr = lower;
-  fahrf = lower;
-  while (fahr <= upper) {
-    celsius = 5 * (fahr  - 32) / 9;
-    celsiusf = 5 * (fahrf  - 32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
-    printf("%3d %6d\n", fahr, celsius); // 指明打印宽度    
-    printf("%3.0f %6.1f\n", fahrf, celsiusf); // 浮点数版本    
-    fahr = fahr + step;
-    fahrf = fahrf + step;
-  }
+//   fahr = lower;
+//   fahrf = lower;
+//   while (fahr <= upper) {
+//     celsius = 5 * (fahr  - 32) / 9;
+//     celsiusf = 5 * (fahrf  - 32) / 9;
+//     printf("%d\t%d\n", fahr, celsius);
+//     printf("%3d %6d\n", fahr, celsius); // 指明打印宽度    
+//     printf("%3.0f %6.1f\n", fahrf, celsiusf); // 浮点数版本    
+//     fahr = fahr + step;
+//     fahrf = fahrf + step;
+//   }
+// }
+int main () {
+  float celsius;
+  printf("摄氏与华氏温度对照表\n");
+
+  for (celsius = 300.0; celsius > 0.0; celsius = celsius - 10.0)
+    printf("%3.0f %6.2f\n", celsius, (celsius / 5.0 * 9.0 + 32));
 }
