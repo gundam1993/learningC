031cb9999a9b99ee9802bdbb0fc8080c9aa3c8a8
huangerdong <3498103680@qq.com>
2017-11-26 10:36:49 +0800

    update

--- temperatureConversion.c
+++ temperatureConversion.c
@@ -2,6 +2,7 @@
 /*打印华氏与摄氏温度对照表*/
 int main() {
   int fahr, celsius;
+  float fahrf, celsiusf; // 浮点数
   int lower, upper, step;
 
   lower = 0; // 温度的下限
@@ -9,9 +10,14 @@ int main() {
   step = 20; // 步长
 
   fahr = lower;
+  fahrf = lower;
   while (fahr <= upper) {
     celsius = 5 * (fahr  - 32) / 9;
+    celsiusf = 5 * (fahrf  - 32) / 9;
     printf("%d\t%d\n", fahr, celsius);
+    printf("%3d %6d\n", fahr, celsius); // 指明打印宽度    
+    printf("%3.0f %6.1f\n", fahrf, celsiusf); // 浮点数版本    
     fahr = fahr + step;
+    fahrf = fahrf + step;
   }
 }
